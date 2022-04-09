ae2b311dcb28a5dad1bc5a319d88fcdbc718b398
Truong Phuc Hung <truong_phuc_hung@yahoo.com.vn>
2022-04-09 21:20:59 +0700

    logic with params

--- pages/docs/[...params].js
+++ pages/docs/[...params].js
@@ -5,7 +5,12 @@ import {useRouter} from "next/router";
 const Doc = () => {
   const router = useRouter();
   const { params = [] } = router.query
-  console.log(params)
+  // handle mock logic with params
+  if (params.length === 2) {
+    return <div>Using for feature {params[0]} of product {params[1]}</div>
+  } else if (params.length === 1) {
+    return <div>Using for only route --- for feature {params[0]}</div>
+  }
   return (
     <div>Doc page for catch all routes</div>
   )

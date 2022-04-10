acd7bd1e7f840e951d7a20cd3288408446905ef7
Truong Phuc Hung <truong_phuc_hung@yahoo.com.vn>
2022-04-10 23:04:04 +0700

    navigate page using Link component of 'next/router'

--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -1,6 +1,10 @@
+import { useRouter } from "next/router";
+
 const ProductId = () => {
+  const router = useRouter();
+  const { id } = router.query;
   return (
-    <div>Product Id page</div>
+    <div>Product Id page of {id}</div>
   )
 }
 
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,7 +1,14 @@
 // when product folder is created, index.js file will be presented with "/product" routing
+import Link from 'next/link'
 const Product = () => {
   return (
-    <div>Product page</div>
+    <>
+      <Link href={'/'}>Home page</Link>
+      <div>Product page</div>
+      <Link href={'/product/1'}>
+        Product 1 page
+      </Link>
+    </>
   )
 }
 

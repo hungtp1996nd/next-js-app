155cddd98bec6b3c5374c17c46d8ff2847f163e6
Truong Phuc Hung <hungtp@vnext.com.vn>
2022-04-14 11:44:46 +0700

    set fallback is true - create fallback page (common using spinner loading)

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,6 +1,9 @@
+import { useRouter } from "next/router";
 import { getPathFromLimit } from "../../lib/post";
 
 const Post = ({ post }) => {
+  const router = useRouter()
+  if (router.isFallback) return <h2>Loading...</h2>
   return (
     <>
       <h2>{post.title}</h2>
@@ -17,7 +20,8 @@ export async function getStaticPaths() {
     paths,
     // fallback contain 3 values: false, true, blocking
     // when fallback set to false => if paths params is not exist => return 404 page, fallback false paths returned will be rendered to HTML file with corresponding "id"
-    fallback: false,
+    fallback: true,
+    // when fallback set to true => page fallback into line 6 will be showing to wait the full page has done by initial rendered.
   }
 }
 

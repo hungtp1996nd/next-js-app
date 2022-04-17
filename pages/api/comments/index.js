178db81bb6baf5269183562690d505ba52aa4d57
Truong Phuc Hung <truong_phuc_hung@yahoo.com.vn>
2022-04-17 22:55:21 +0700

    handle POST request of API routes

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,5 +1,16 @@
 import { comments } from "../../../data/comment";
 
 export default function handler(req, res) {
-  res.status(200).json(comments)
+  if (req.method === 'GET') {
+    res.status(200).json(comments)
+  } else if (req.method === 'POST') {
+    const comment = req.body.comment
+    const currentMaxId = Math.max.apply(Math, comments.map(function(o) { return o.id; }))
+    const newComment = {
+      id: currentMaxId + 1,
+      text: comment,
+    }
+    comments.push(newComment)
+    res.status(201).json(comment)
+  }
 }
--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,14 +2,32 @@ import {useState} from "react";
 
 const CommentPage = () => {
   const [comments, setComments] = useState([])
+  const [comment, setComment] = useState('')
+
   const handleLoadComments = async () => {
     const response = await fetch('http://localhost:3000/api/comments')
     const comments = await response.json()
     setComments(comments)
   }
+
+  const handleAddComment = async () => {
+    const response = await fetch('http://localhost:3000/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({ comment }) ,
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    const data = await response.json()
+  }
+
   return (
     <div>
       <button onClick={handleLoadComments}>Load comments</button>
+      <div>
+        <input type="text" value={comment} onChange={(event => setComment(event.target.value))}/>
+        <button onClick={handleAddComment}>Add comment</button>
+      </div>
       {
         comments?.map(comment => <div key={comment.id}>{comment.id} - {comment.text}</div>)
       }

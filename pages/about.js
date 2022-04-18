7bf757065716a846d2e2cce28ba833c5f9ff3806
Truong Phuc Hung <hungtp@vnext.com.vn>
2022-04-18 14:32:56 +0700

    using Head component of NextJS - consider using Image component of NextJS to reduce time by using lazy load

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,16 @@
 import Footer from "../components/Footer";
+import Head from 'next/head'
 
 const About = () => {
-  return <div className='content'>About page</div>
+  return (
+    <>
+      <Head>
+        <title>About page testing with Head component</title>
+        <meta name='description' content='make clear to the page' />
+      </Head>
+      <h1 className='content'>About page</h1>
+    </>
+  )
 }
 
 export default About
